ae69a239ecc5c521fcd3a3ce410f2636b182e7a1
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-05-28 10:06:07 -0500

    load assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "1B1D1F": "#1B1D1F",
+      "282B30": "#282B30",
+      "4E80EE": "#4E80EE",
+      "6C727F": "#6C727F",
+      "D2D5DA": "#D2D5DA",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "hero":'url("/hero-image-wr.jpg")'
       },
     },
   },
